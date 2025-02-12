b295d7b8823ff300e4705fa0678a9426c7aaf2d6
Raunak Sood <raunaksood@Raunaks-MacBook-Pro-7.local>
2025-02-12 11:03:13 -0500

    switch theta1 and theta2 in inverse kienmatic equation

--- kinematics.py
+++ kinematics.py
@@ -6,15 +6,15 @@ class Kinematics:
         self.L2 = L2
 
     # Inputs: x (forward/backward), y (height), L1 (arm 1 length), L2 (arm 2 length)
-    # Outputs: theta1 (angle)
+    # Outputs: theta2 (angle)
     def forward(self, x, y):
         num = x**2 + y**2 - self.L1**2 - self.L2**2
         den = 2 * self.L1 * self.L2
         return math.acos(num / den)
 
-    # Inputs: x (forward/backward), y (height), L1 (arm 1 length), L2 (arm 2 length), theta1 (angle)
-    # Outputs: theta2 (angle)
-    def inverse(self, x, y, theta1):
+    # Inputs: x (forward/backward), y (height), L1 (arm 1 length), L2 (arm 2 length), theta2 (angle)
+    # Outputs: theta1 (angle)
+    def inverse(self, x, y, theta2):
         first = math.atan2(y, x)
-        second = math.atan2(self.L2 * math.sin(theta1), self.L1 + self.L2 * math.cos(theta1))
+        second = math.atan2(self.L2 * math.sin(theta2), self.L1 + self.L2 * math.cos(theta2))
         return first - second
